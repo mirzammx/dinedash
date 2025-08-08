35ecf451611c1f96ef2da055e8a984f1d703cb93
mohammed mirza <1ds22cb032@dsce.edu.in>
2025-08-08 17:13:06 +0530

    Initial commit with dine dash project

---
+++ FoodFrenzy-master/src/test/java/com/example/demo/FoodFrenzyApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodFrenzyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
+spring.datasource.password=12345;
